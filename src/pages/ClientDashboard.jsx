324f62e7fb532ff523936dcddad2ce00d492e68e
drew <drewtuannguyen2003@gmail.com>
2025-02-21 18:57:54 -0500

    logout changes

--- src/pages/ClientDashboard.jsx
+++ src/pages/ClientDashboard.jsx
@@ -1,45 +1,85 @@
 import styles from "./ClientDashboard.module.css"
 import {useEffect, useState } from "react";
+import {useNavigate} from "react-router-dom"
 import axios from "axios";
 import Loading from "../components/Loading"
 import ErrorMessage from "../components/ErrorMessage";
+import ButtonNavLink from "../components/ButtonNavLink";
 //import { getCookie } from "../functions/GetCookie";
 //import ListItem from "../components/ListItem";
 import ScheduleAppointmentList from "../components/ScheduleAppointmentList"
 
 
-
-function getCookie(name) {
-  const cookies = document.cookie.split(';');
-  for(let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].trim(); // trims the whitespace in the array of characters
-    
-    // picks which substring starts with the token name plus the equal sign
-    if (cookie.startsWith(name + '=')) { 
-      return cookie.substring(name.length + 1); // returns the token not counting "name="
-    }
-  }
-  return null;
-}
-
-
-const token = getCookie("token");
-
-
 // Client Dashboard component that allows the user to view newly created appointments and past visits.
 export default function ClientDashboard() {
-
+  // state variables
   const [isDataLoading, setIsDateLoading] = useState(false);
   const [error, setError] = useState(false);
+  const [data, setData] = useState([]);
+
+  const date = new Date();
+  console.log(date);
+  console.log(date.getDay());
+  console.log(date.getFullYear());
+
+  // color for the navigation button on this page (Back button).
+  const backButtonColor = "orange";
+
+  // navigator
+  const navigator = useNavigate();
+
+  // source https://github.com/PranayChavhan/GSport-V1
+  // get the cookie string
+  const getCookieToken = (name)=> {
+    const cookies = document.cookie.split(';');
+    for(let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim(); // trims the whitespace in the array of characters
+      
+      // picks which substring starts with the token name plus the equal sign
+      if (cookie.startsWith(name + '=')) { 
+        return cookie.substring(name.length + 1); // returns the token not counting "name="
+      }
+    }
+    return null;
+  }
+
+  // return the cookie name
+  const getCookieName = (name)=> {
+    const cookies = document.cookie.split(';');
+    for(let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].trim(); // trims the whitespace in the array of characters
+      
+      // picks which substring starts with the token name plus the equal sign
+      if (cookie.startsWith(name)) { 
+        console.log(name);
+        return name; // returns the token not counting "name="
+      }
+    }
+    return null;
+  }
+
+  const deleteCookieOnSignOut = async() => {
+
+    const cookieName = getCookieName("token");
+    
+    document.cookie = `${cookieName}=; expires=Wed, 02 Feb 1960 10:00:01 GMT;`;
+    navigator("/")
+    console.log(name);
+    
+  }
+    
 
   
-  const [data, setData] = useState([]);
+  
+  
+
 
   // The data is fetched on the client dashboard pages initial render (when the page shows up)
   // This is because of the useEffect hook which allows for these actions to ocurr.
   useEffect(() => {
     const fetchAppointments = async() => {
 
+      const token = getCookieToken("token");
       setIsDateLoading(true);
       const apiURL = 'https://api.vpbackendapi.com:5000/api/appointments';
       try {
@@ -75,7 +115,16 @@ export default function ClientDashboard() {
  
   return (
     <div className={styles.container}>
-      <h1>Client Dashboard</h1>
+
+      <div className={styles.navbar}>
+        <div className={styles.navbar_item}>
+          <ButtonNavLink to="/scheduling" id={styles.goBackButton} color={backButtonColor}>⬅ Back</ButtonNavLink>
+        </div>
+        <div><h1>Client Dashboard</h1></div>
+        <div className={styles.navbar_item}>
+          <button className={styles.sign_out_button} onClick={deleteCookieOnSignOut}>Sign Out</button>
+        </div>
+      </div>
       
       <div className={styles.inner_container}>
 
